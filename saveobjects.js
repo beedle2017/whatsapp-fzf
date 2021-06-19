4af5ad96cd5a29f34bb64e170075822db973f8c7
beedle2017 <choudhuriarka@gmail.com>
2021-06-19 20:28:41 +0530

    Sending message functionality added

--- saveobjects.js
+++ saveobjects.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 
 // const { exec, spawnSync } = require("child_process");
 
-// const spawn = require('await-spawn');
+const spawn = require('await-spawn');
 
 const shell = require('shelljs');
 
@@ -27,13 +27,13 @@ async function middle(person)
 {
     person=person.substring(0,person.length-1);
 
-    const messages=await chats[person].fetchMessages({limit: 5});
+    const messages=chatmessages[person];
 
     const setupFile = fs.writeFileSync(`curr.txt`, person+'\n\n');
 
     for (message of messages)
     {
-        let addition = message.body+'\n\n';
+        let addition = message+'\n\n';
         const data = fs.writeFileSync(`curr.txt`, addition, { flag: 'a+' });
     }
 
@@ -42,6 +42,27 @@ async function middle(person)
     if(interaction.localeCompare('<--Back\n')==0)
     {
         start();
+    }
+    else if(interaction.localeCompare(`Past Messages\n`)==0)
+    {
+        middle(person+'\n');
+    }
+    else if(interaction.localeCompare(`Send Message\n`)==0)
+    {
+        const make_message = await spawn('vim',['message.txt'],{stdio:'inherit'});
+        let new_message = fs.readFileSync('./message.txt', {encoding:'utf8'});
+
+        let numberofperson = chats[person].id._serialized;
+
+        const throwaway = chatmessages[person].shift();
+
+        chatmessages[person].push(new_message);
+
+        const msg= await client.sendMessage(numberofperson,new_message);
+
+        console.log(chatmessages[person],person,numberofperson,new_message);
+
+        middle(person+'\n');
     }
     else
     {
@@ -68,10 +89,23 @@ async function body()
     await client.initialize();   
     let arr = await client.getChats();
 
-    for (chatobj of arr)
+    for (let i=0; i<5; i++)
     {
+        let chatobj = arr[i];
+
         fzfstring=fzfstring+chatobj.name+'\\n';
         chats[chatobj.name]=chatobj;
+
+        let messages=await chatobj.fetchMessages({limit: 5});
+
+        chatmessages[chatobj.name]=[];
+
+        console.log(`Processing ${chatobj.name}`);
+
+        for(message of messages)
+        {
+            chatmessages[chatobj.name].push(message.body);
+        }
     }
 
     start();
@@ -79,4 +113,5 @@ async function body()
 
 let fzfstring='';
 let chats={};
+let chatmessages={};
 body();
